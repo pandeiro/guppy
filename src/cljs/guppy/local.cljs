f42e3e08af95038231e299bfb499bb0d658c2892
Murphy McMahon <pandeiro@gmail.com>
2014-10-01 22:49:04 -0300

    fix out of scope

--- src/cljs/guppy/local.cljs
+++ src/cljs/guppy/local.cljs
@@ -2,9 +2,10 @@
   (:require [cljs.reader :refer [read-string]]))
 
 (defn sync! [k r o n]
-  (let [data {:old (:data o) :new (:data n)}]
+  (let [data {:old (:data o) :new (:data n)}
+        storage-key (name (:name n))]
     (when (not= (:old data) (:new data))
-      (.setItem js/localStorage (name (:name @app-state)) (pr-str (:new data))))))
+      (.setItem js/localStorage storage-key (pr-str (:new data))))))
 
 (defn restore! [r]
   (when-let [data (not-empty (.getItem js/localStorage (name (:name @app-state))))]
